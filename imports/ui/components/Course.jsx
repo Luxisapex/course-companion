3402f01c91f440cb6f63ed1e4b56b78a32bdc5cd
Fredrik Löfberg <frelo206@student.liu.se>
2016-05-05 19:49:24 +0200

    Made trashcans clickable

--- imports/ui/components/Course.jsx
+++ imports/ui/components/Course.jsx
@@ -14,6 +14,9 @@ import React from 'react';
 
 export default class Course extends React.Component {
 
+	removeCourse(event) {
+		return console.log("hej");
+	}
 
 	render() {
 		return (
@@ -21,7 +24,12 @@ export default class Course extends React.Component {
 				<li className="course-code"><a href="/main">{this.props.code}</a></li>
 				<li className="course-name">{this.props.name}</li>
 				<li className="check-course"><input type="checkbox"/></li>
-				<li className="remove-course"><i className="fa fa-trash" aria-hidden="true"></i></li>
+				<li className="remove-course">
+					<a href=""lassName="btn btn-primary" 
+					onClick={this.removeCourse}>
+					<i className="fa fa-trash" aria-hidden="true"></i>
+					</a>
+				</li>
 			</ul>
 		);
 	}
