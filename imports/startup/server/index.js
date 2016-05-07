a62b4790f610dfe55ac812d2f1fc2309e314507d
Joel Garplind <joega818@student.liu.se>
2016-05-07 10:10:37 +0200

    imports current courses correctly to avoid error

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,7 +1,7 @@
 import { Meteor } from 'meteor/meteor';
 
 // Import Collections (pieces of the DB) and its publications
-import '../../api/courses/courses.js';
+import {Courses} from '../../api/courses/courses.js';
 import '../../api/courses/server/publications.js';
 
 // Not relevant for now
