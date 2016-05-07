0a1e0103bb68b460ce7c9cc8e11c166f03eb3747
Joel Garplind <joega818@student.liu.se>
2016-05-07 09:57:19 +0200

    basic course data supplied if DB is empty

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -9,5 +9,26 @@ import '../../api/courses/server/publications.js';
 
 // Somehow necessary
 Meteor.startup(() => {
-	
+	if (Courses.find().count() === 0) {
+		const data = [
+			{
+				name: "Ekonomisk grundkurs",
+				code: "TEIE17",
+				points: 6
+			},
+			{
+				name: "Matematisk grundkurs",
+				code: "TATA31",
+				points: 6
+			}
+		];
+
+		data.forEach((list) => {
+			Lists.insert({
+				name: list.name,
+				code: list.code,
+				points: list.points
+			});
+		});
+	}
 });
