7ee65beaba5608c60ecc34e5cc93085dbcc29637
Joel Garplind <joega818@student.liu.se>
2016-05-09 22:14:54 +0200

    now re-routes on login but needs more work

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -4,6 +4,9 @@
 
 import React from 'react';
 
+// Renders empty object so not very useful.. try to fix
+// import Accounts from 'meteor/gwendall:auth-client-callbacks';
+
 // "Mounts" react component on a Layout
 import { mount } from 'react-mounter';
 
@@ -13,6 +16,27 @@ import { MainLayout } from '../../ui/layouts/MainLayout.jsx';
 
 // Import all the components used in the routes
 import CourseList from '../../ui/components/CourseList.jsx';
+
+// Moves user when login/out action is taken
+// Missing Accounts for now fix later
+// if(Meteor.isClient) {
+// 	Accounts.onLogin(function() {
+// 		FlowRouter.go('main');
+// 	});
+
+// 	Accounts.onLogout(function() {
+// 		FlowRouter.go('home');
+// 	});
+// }
+
+// Will do for now, but only works on login and throws console errors
+if(Meteor.isClient) {
+	Tracker.autorun(function() {
+		if(Meteor.userId()) {
+			FlowRouter.go('main');
+		}
+	});
+}
 
 // Only lets logged in users access anything but the home route
 FlowRouter.triggers.enter([function(context, redirect){
@@ -24,9 +48,10 @@ FlowRouter.triggers.enter([function(context, redirect){
 FlowRouter.route('/', {
 	name: 'home',
 	action() {
-		mount(HomeLayout, {
-			content: (<CourseList />)
-		});
+		if(Meteor.userId()) {
+			FlowRouter.go('main');
+		}
+		mount(HomeLayout);
 	}
 });
 
--- imports/ui/layouts/MainLayout.jsx
+++ imports/ui/layouts/MainLayout.jsx
@@ -6,12 +6,12 @@ import React from 'react';
 import Navbar from '../components/Navbar.jsx';
 import InputField from '../components/InputField.jsx';
 import SearchCourse from '../components/SearchCourse.jsx';
+import AccountsUIWrapper from '../components/AccountsUIWrapper.jsx';
 
 export const MainLayout = ({content}) => (
 	<div className="main-layout">
+		<AccountsUIWrapper />
 		<Navbar />
 		{content}
-		<InputField placeholder="Industriell ekonomi" />
-		<SearchCourse />
 	</div>
 );
