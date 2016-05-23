8512abd9abbc9e1617b8e8f6146289ce8536df69
Fredrik Löfberg <frelo206@student.liu.se>
2016-05-23 20:48:38 +0200

    Fixed search bar to only display on course code, make sure to implement on course name as well

--- imports/ui/components/InputField.jsx
+++ imports/ui/components/InputField.jsx
@@ -4,38 +4,21 @@
 import React from 'react';
 import TrackerReact from 'meteor/ultimatejs:tracker-react';
 
+//Importing these two for trying courses with search bar
+import { Courses } from '../../api/courses/courses.js';
+
+import Course from './Course.jsx';
+//
+
 import { addEducation, addTech, addMaster } from '../../api/users/methods.js';
 
 export default class InputField extends TrackerReact(React.Component) {
-
-	// State depends on what user has input so far. Edu > Tech > Master > None
-	// constructor() {
-	// 	super();
-
-	// 	// this.state = {
-	// 	// 	subscription: {
-	// 	// 		users: Meteor.subscribe('userData')
-	// 	// 	}
-	// 	// }
-
-	// 	if(Meteor.user().master) {
-	// 		this.state = {
-	// 			currentInput: ''
-	// 		}
-	// 	} else if (Meteor.user().tech) {
-	// 		this.state = {
-	// 			currentInput: 'master'
-	// 		}
-	// 	} else if (Meteor.user().education) {
-	// 		this.state = {
-	// 			currentInput: 'tech'
-	// 		}
-	// 	} else {
-	// 		this.state = {
-	// 			currentInput: 'edu'
-	// 		}
-	// 	}
-	// }
+	constructor() {
+		super();
+		this.state = {
+			search: 'TEIE17'
+		};
+	}
 
 	handleInput(event) {
 		event.preventDefault();
@@ -67,45 +50,44 @@ export default class InputField extends TrackerReact(React.Component) {
 
 
 
-		// addEducation.call({
-		// 	userId: Meteor.userId(),
-		// 	education: text
-		// });
 
 		this.refs.input.value = "";
 		
-		// Next input (dynamic somehow)
-		// this.state.currentInput = 'tech';
-		// this.refs.input.placeholder = this.state.currentInput;
-		// this.refs.input.placeholder = "...";
+	}
+
+	updateSearch(event) {
+		this.setState({search: event.target.value.substr(0, 20)})
+	}	
+
+	courses() {
+		return Courses.find({}).fetch();
+	}
+
+	coursesSelected() {
+		console.log(this.state.search);
+		return Courses.find({code: this.state.search.toUpperCase()});
 	}
 
 	render() {
-		if(Meteor.user().master) {
-			this.state = {
-				currentInput: ''
-			}
-		} else if (Meteor.user().tech) {
-			this.state = {
-				currentInput: 'master'
-			}
-		} else if (Meteor.user().education) {
-			this.state = {
-				currentInput: 'tech'
-			}
-		} else {
-			this.state = {
-				currentInput: 'edu'
-			}
-		}
 		return (
-			<form onSubmit={this.handleInput.bind(this)}> 
-				<input
-					type="text"
-					ref="input"
-					placeholder={this.state.currentInput}
-				/>
-			</form>
+			<div>
+				<input type="text" value={this.state.search} 
+					onChange={this.updateSearch.bind(this)} />
+				<ul>	
+						
+						{console.log(this.state)}
+
+
+						{Meteor.user().master}
+						{this.state.currentInput}
+						{this.coursesSelected().map((course) => {
+							return <Course key={course._id} course={course} />
+						})}
+
+						
+				</ul>
+			</div>
+
 		);
 	}
 };
