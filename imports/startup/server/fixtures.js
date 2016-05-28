d73dbbc1883651c9fe62deca7e9e4654f6b7b87e
Joel Garplind <joega818@student.liu.se>
2016-05-28 22:40:25 +0200

    educations include proper course objects

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -6,85 +6,98 @@ Meteor.startup(() => {
 	// Make sure no login keys persist over server restart
 	Meteor.users.update({}, {$set : { "services.resume.loginTokens" : [] }}, {multi:true});
 	// Fill DB with this data if empty
+	if (Courses.find().count() === 0) {
+		const courseData = [
+			{
+				name: "Ekonomisk grundkurs",
+				code: "TEIE17",
+				points: 8,
+			},
+			{
+				name: "Matematisk grundkurs",
+				code: "TATA31",
+				points: 8
+			},
+			{
+				name: "Fysiologisk grundkurs",
+				code: "TFAE12",
+				points: 8,
+			},
+			{
+				name: "Linjär algebra",
+				code: "TATA32",
+				points: 6
+			},
+			{
+				name: "Envariabelanalys 1",
+				code: "TATA41",
+				points: 6
+			},
+			{
+				name: "Envariabelanalys 2",
+				code: "TATA42",
+				points: 6
+			},
+			{
+				name: "Flervariabelanalys",
+				code: "TATA43",
+				points: 8
+			},
+			{
+				name: "Industriell organisation",
+				code: "TEIO61",
+				points: 8
+			},
+			{
+				name: "Programmering i ADA",
+				code: "TDDD95",
+				points: 6
+			},
+			{
+				name: "Godtycklig kurs",
+				code: "TEIE55",
+				points: 6
+			}
+		];
+
+		courseData.forEach((course) => {
+			Courses.insert({
+				name: course.name,
+				code: course.code,
+				points: course.points
+			});
+		});
+	}
+
 	if (Educations.find().count() === 0) {
-		const data = [
+		const educationData = [
 			{
 				name: 'Industriell Ekonomi',
 				nickname: 'I',
 				courses: [
-					{
-						name: "Ekonomisk grundkurs",
-						code: "TEIE17",
-						points: 8,
-					},
-					{
-						name: "Matematisk grundkurs",
-						code: "TATA31",
-						points: 8
-					}
+					"TEIE17",
+					"TATA31"
 				]
 			},
 			{
 				name: 'Teknisk Fysik',
 				nickname: 'Y',
 				courses: [
-					{
-						name: "Fysiologisk grundkurs",
-						code: "TFAE12",
-						points: 8,
-					},
-					{
-						name: "Matematisk grundkurs",
-						code: "TATA31",
-						points: 8
-					}
+					"TFAE12",
+					"TATA31"
 				]
 			},
-			// {
-			// 	name: "Linjär algebra",
-			// 	code: "TATA32",
-			// 	points: 6
-			// },
-			// {
-			// 	name: "Envariabelanalys 1",
-			// 	code: "TATA41",
-			// 	points: 6
-			// },
-			// {
-			// 	name: "Envariabelanalys 2",
-			// 	code: "TATA42",
-			// 	points: 6
-			// },
-			// {
-			// 	name: "Flervariabelanalys",
-			// 	code: "TATA43",
-			// 	points: 8
-			// },
-			// {
-			// 	name: "Industriell organisation",
-			// 	code: "TEIO61",
-			// 	points: 8
-			// },
-			// {
-			// 	name: "Programmering i ADA",
-			// 	code: "TDDD95",
-			// 	points: 6
-			// },
-			// {
-			// 	name: "Godtycklig kurs",
-			// 	code: "TEIE55",
-			// 	points: 6
-			// }
 		];
-		// Inserts data
-		data.forEach((education) => {
+		// Fills array with Courses, and creates Education with that array
+		educationData.forEach((education) => {
 			
 			let mandatoryCourses = [];
 
 			education.courses.forEach((course) => {
-				mandatoryCourses.push(course);
+				mandatoryCourses.push(
+					Courses.findOne({code: course})
+				);
 			});
-			console.log(mandatoryCourses);
 
 			Educations.insert({
 				name: education.name,
